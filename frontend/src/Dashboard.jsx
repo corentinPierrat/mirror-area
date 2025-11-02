c0511915a353ee543a9829b77872dc353bdb69f1
yanis <yanis.senovic@epitech.eu>
2025-11-02 22:07:08 +0100

    [Fix] bad import

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import HeaderDashboard from './components/HeaderDashboard';
 import MyWorkflow from "./components/MyWorkflow";
 import Footer from './components/Footer';
-import videoBg from "../public/bg-video.mp4";
 import ProfileBubble from "./components/ProfileBubble";
 import styles from "./styles/Dashboard.module.css";
 
@@ -49,13 +48,13 @@ export default function Dashboard() {
   return (
     <div className={styles.dashboardContainer}>
       <video
-              className={styles.videoBackground}
-              src={videoBg}
-              autoPlay
-              loop
-              muted
-              playsInline
-            />
+        className={styles.videoBackground}
+        src="/bg-video.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
       <div className={styles.dashboardBody}>
         <aside className={styles.sidebar}>
           <HeaderDashboard />
